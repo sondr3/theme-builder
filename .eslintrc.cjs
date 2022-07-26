38896ea6ef32f522f9953ae53f6ae7c318957205
Sondre Aasemoen <sondre@eons.io>
2022-07-26 15:43:17 +0200

    In the beginning there was darkness

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: ["@sondr3/eslint-config/typescript"],
+  plugins: ["svelte3"],
+  ignorePatterns: ["*.cjs"],
+  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+  settings: {
+    "svelte3/typescript": () => require("typescript"),
+  },
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+};
