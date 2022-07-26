f2709695410bc037b6e56f20b45a98a993d433d4
Sondre Aasemoen <sondre@eons.io>
2022-07-26 20:08:38 +0200

    Create empty state for colors

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -44,4 +44,27 @@
 
 {#each $colors as color, i (i)}
   <ColorRow {color} />
+{:else}
+  <button
+    type="button"
+    on:click={addColor}
+    class="mt-2 relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="mx-auto h-12 w-12 text-gray-400"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      stroke-width="2"
+      aria-hidden="true"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+      />
+    </svg>
+    <span class="mt-2 block text-sm font-medium text-gray-900"> Add a color </span>
+  </button>
 {/each}
